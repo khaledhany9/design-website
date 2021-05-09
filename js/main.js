d9a72640dfec827c8eda4f7bc827bce43401a802
Khaled Hany <khaledhanyyousef@gmail.com>
2021-05-09 21:42:04 +0200

    Design

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+
+    $('.fa-bars').click(function(){
+      $(this).toggleClass('fa-times');
+      $('.navbar').toggleClass('nav-toggle');
+    });
+  
+    $(window).on('load scroll',function(){
+      $('.fa-bars').removeClass('fa-times');
+      $('.navbar').removeClass('nav-toggle');
+  
+      if($(window).scrollTop() > 100){
+        $('.header').css({'background':'#6C5CE7','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+      }else{
+        $('.header').css({'background':'none','box-shadow':'none'});
+      }
+    });
+  
+  
+    $('.accordion-header').click(function(){
+      $('.accordion .accordion-body').slideUp();
+      $(this).next('.accordion-body').slideDown();
+      $('.accordion .accordion-header span').text('+');
+      $(this).children('span').text('-');
+
+      
+    });
+  
+    
+
+    
+  
+  });
+
+    $(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+        var scroll;
+
+        if($(window).scrollTop()==0){
+            scroll =  ($target.offset().top) - 160
+        }else{
+            scroll =  ($target.offset().top) - 60
+        }
+        $('html, body').stop().animate({
+            'scrollTop': scroll
+        }, 900, 'swing', function () {
+            //window.location.hash = target;
+        });
+    });
+});
+
+
